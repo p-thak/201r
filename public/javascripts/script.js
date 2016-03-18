4e23881af24980c13bec8f4f4b31f8fa30cb44d1
dmand91 <dmand91@gmail.com>
2016-03-18 04:06:44 +0000

    added options functionality

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,34 +1,6 @@
+
+
 $( document ).ready(function() {
-
-     $("#generateFlag").click(function() {
-           console.log("Key being pressed");
-
-          var url = "../getrandomcountry";
-        $.getJSON(url,function(data) {
-           
-            console.log(data); 
-	    //var json = JSON.parse(data.json);
-
-//	    console.log(json);
-	    var countryName=  data[0].Name;
-	    var countryCode = data[1].Code;
- 	    
-	    console.log(countryCode);
-	    console.log(countryName);
-            var url = "/images/"+countryCode.toLowerCase()+".png";	
-		console.log(url);	
-            $("#flagImage").html("<img src='"+url+"'>");
-             $("#countryName").html("<p>"+countryName+"</p>");
-          })
-          .done(function() { console.log('getJSON request succeeded!'); })
-          .fail(function(jqXHR, textStatus, errorThrown) {
-            console.log('getJSON request failed! ' + textStatus);
-            console.log("incoming "+jqXHR.responseText);
-          })
-          .always(function() { console.log('getJSON request ended!');
-          })
-          .complete(function() { console.log("complete"); });
-        });
 
 
 
@@ -62,7 +34,27 @@ function userFetcher ($http) {
 
 function mainCtrl ($scope, userFetcher) {
 
-  $scope.user = []
+  $scope.user = [];
+  $scope.optionsList = [];
+  $scope.flagShowing = false;
+  
+ $scope.generateOptions = function() {
+ 	  $scope.optionsList = [];     
+        $scope.optionsList.push($scope.countryName);
+  	
+        //var newOption =grabNewOption(); 
+	//console.log(newOption);
+    for(var i =0; i<5; i++){	
+	grabNewOption(function(result){
+		console.log("new option: "+result);
+
+             $scope.optionsList.push(result);	
+		console.log("size of array: "+$scope.optionsList.length);	
+      	     console.log($scope.optionsList);
+	});
+      }
+  }
+ 
 
    $scope.addUser = function() {
       var formData = {name:$scope.Name,highScore:$scope.highScore,currentScore:$scope.currentScore};
@@ -75,5 +67,66 @@ function mainCtrl ($scope, userFetcher) {
     .then(function (data) {
       $scope.user = data
     })
+
+ function grabNewOption(callback){
+ console.log("grabbing new option");
+
+          var url = "../getrandomcountry";
+        $.getJSON(url,function(data) {
+
+            console.log(data);
+            //var json = JSON.parse(data.json);
+
+//          console.log(json);
+            var countryNameOption=  data[0].Name;
+
+            console.log(countryNameOption);
+		callback(countryNameOption);
+          })
+          .done(function() { console.log('getJSON request succeeded!'); })
+          .fail(function(jqXHR, textStatus, errorThrown) {
+            console.log('getJSON request failed! ' + textStatus);
+            console.log("incoming "+jqXHR.responseText);
+          })
+          .always(function() { console.log('getJSON request ended!');
+          })
+          .complete(function() { console.log("complete"); });
+        //});
+
+
+}
+	
+ $("#generateFlag").click(function() {
+           console.log("Key being pressed");
+
+          var url = "../getrandomcountry";
+        $.getJSON(url,function(data) {
+
+            console.log(data);
+            //var json = JSON.parse(data.json);
+
+//          console.log(json);
+            $scope.countryName=  data[0].Name;
+            var countryCode = data[1].Code;
+
+            console.log(countryCode);
+            console.log(countryName);
+            var url = "/images/"+countryCode.toLowerCase()+".png";
+                console.log(url);
+            $("#flagImage").html("<img src='"+url+"'>");
+             $("#countryName").html("<p>"+countryName+"</p>");
+
+	    $scope.flagShowing=true;
+	    $scope.generateOptions();	
+          })
+          .done(function() { console.log('getJSON request succeeded!'); })
+          .fail(function(jqXHR, textStatus, errorThrown) {
+            console.log('getJSON request failed! ' + textStatus);
+            console.log("incoming "+jqXHR.responseText);
+          })
+          .always(function() { console.log('getJSON request ended!');
+          })
+          .complete(function() { console.log("complete"); });
+        });
 
 }
